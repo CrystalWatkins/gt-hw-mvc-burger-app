52897e05940e72f466e01ea4827da7654c3c3eb8
Crystal <crystalwatkins428@gmail.com>
2020-07-13 17:34:56 -0400

    created folders

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("hamburger");
+INSERT INTO burgers (burger_name) VALUE ("bacon cheeseburger");
+INSERT INTO burgers (burger_name) VALUE ("double cheeseburger");
