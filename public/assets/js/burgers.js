e49e4ab9e67bb675623ed7fe02ef7ac789f986a4
Crystal <crystalwatkins428@gmail.com>
2020-07-14 10:38:08 -0400

    got css working and updated js file

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,7 +1,7 @@
 $(function() {
     $(".change-devoured").on("click", function(event) {
       var id = $(this).data("id");
-      var newDevoured = $(this).data("newdevoured");
+      var newDevoured = $(this).data("devoured");
   
       var newDevouredState = {
         devoured: newDevoured
@@ -25,8 +25,8 @@ $(function() {
       event.preventDefault();
   
       var newBurger = {
-        burger_name: $("#burg").val().trim(),
-        devoured: $("[name=devoured]:checked").val().trim()
+        name: $("#burg").val().trim(),
+        devoured: $("[name=devoured]").val().trim()
       };
   
       // Send the POST request.
